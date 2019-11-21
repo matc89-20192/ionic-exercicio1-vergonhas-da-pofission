0b60836b841e80bd038eca67f337a663601482dd
Tom <antoniofdiass@gmail.com>
2019-11-21 13:01:27 -0300

    Update page1
    Solves the first exercise, removes the second page and adds the trademarked joke.

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -8,8 +8,17 @@ import { IonicPage, NavController } from 'ionic-angular';
 })
 export class Page1 {
 
+  public username = "Mundo"
+  public new_username = "";
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  greet() {
+    if (this.new_username == "Eduardo") this.username = "Philipe";
+    else if (this.new_username == "Manoel") this.username = "Magnata";
+    else this.username = this.new_username ? this.new_username : this.username;
   }
 
 }
